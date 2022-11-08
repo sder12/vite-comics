0751240fedecf48155cc7f17d7f0ac7d5d4bc3f7
sder12 <sara.cetto@gmail.com>
2022-11-08 14:37:15 +0100

    Vue components + link header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 
 <script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+    components:
+    {
+        AppHeader,
+    }
+}
 </script>
 
 <template>
     <h1>DC COMICS</h1>
+    <AppHeader />
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "AppHeader"
+}
+</script>
+
+<template>
+    <h2>header</h2>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
